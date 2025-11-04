9cdac8152533b321fe6017c636c5eabce0ac4638
Rahul Rodrigues <rodrigues.rahul77@gmail.com>
2025-11-04 18:57:41 -0500

    initial files

---
+++ assignment3.py
@@ -0,0 +1,22 @@
+
+class td_qlearning:
+
+  alpha = 0.10
+  gamma = 0.90
+
+  def __init__(self, directory):
+    # directory is the path to a directory containing trials through state space
+    # Return nothing
+
+  def qvalue(self, state, action):
+    # state is a string representation of a state
+    # action is an integer representation of an action
+
+    # Return the q-value for the state-action pair
+    return q
+
+  def policy(self, state):
+    # state is a string representation of a state
+
+    # Return the optimal action (as an integer) under the learned policy
+    return a
